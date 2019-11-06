20e91b318e7c32057706b1e0c4d8d34406eaf712
dora <31735614+dora1998@users.noreply.github.com>
2019-11-06 14:32:25 +0900

    完成

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ load_dotenv()
 WEBHOOK_URL = os.environ["WEBHOOK_URL"]
 APPLE_API_URL = os.environ["APPLE_API_URL"]
 POSTCODE = os.environ["POSTCODE"]
+PRODUCT_CODE = 'MWP22J/A'
 
 def get_json():
   params = {
-      'parts.0': 'MWP22J/A',
+      'parts.0': PRODUCT_CODE,
       'location': POSTCODE
   }
 
@@ -21,6 +22,21 @@ def get_json():
 
 def main():
   stock_data = get_json()
-  print(json.dumps(stock_data, indent=4))
+  if stock_data['head']['status'] != '200':
+    print(f"StatusCodeError: {stock_data['head']['status']}")
+    return
+
+  stores = stock_data['body']['stores']
+  output = ""
+  for store in stores:
+    pickup_quote = store['partsAvailability'][PRODUCT_CODE]['storePickupQuote']
+    print(f"{store['storeName']}: {pickup_quote}")
+    if '本日' in pickup_quote:
+      output += f"{pickup_quote}\n"
+
+  if output != "":
+    output = "🎉受け取れる在庫があります！\n" + output
+    print(f"\n{output}")
+    r = requests.post(WEBHOOK_URL, data=json.dumps({'text': output}))
 
 main()
